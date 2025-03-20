f3246c17ceb518abd3382f342db5b15163c9a5c0
anita <anita.rend@gmail.com>
2025-03-20 20:43:43 +0100

    reverse words in the input string

---
+++ src/ReverseWordsInString.java
@@ -0,0 +1,28 @@
+public class ReverseWordsInString {
+
+    // program koji ucitava ulazni string i okrene redoslijed rijeci u tom stringu
+    public static void main(String[] args) {
+
+        String initialString = "Danas je cetvrtak";
+
+        System.out.println("Pocetna recenica:");
+        System.out.println(initialString);
+
+        String[] words = initialString.split("\\s+");
+
+//        for (String word : words) {
+//            System.out.println(word);
+//        }
+
+        System.out.println("Okrenuti redoslijed rijeci u recenici:");
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            stringBuilder.append(words[i]);
+            if (i > 0) {
+                stringBuilder.append(" ");
+            }
+        }
+        System.out.println(stringBuilder);
+    }
+}
